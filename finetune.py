0b12086cb7202e6b5269366c8af3c7bfb68281ff
Huy Khang <maihuykhanghaha@gmail.com>
2026-05-19 11:21:40 +0700

    try remove fp16 on training arguments

--- finetune.py
+++ finetune.py
@@ -95,7 +95,6 @@ if __name__ == "__main__":
         logging_steps=10,
         load_best_model_at_end=True,
         report_to="none",
-        fp16=torch.cuda.is_available(),
         remove_unused_columns=False,
         gradient_checkpointing=True,
         gradient_checkpointing_kwargs={"use_reentrant": False},
