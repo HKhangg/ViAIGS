edf3d727ea885aae762521dc756cd6b1160c71ec
Huy Khang <maihuykhanghaha@gmail.com>
2026-05-19 12:39:48 +0700

    up batch

--- finetune.py
+++ finetune.py
@@ -102,8 +102,8 @@ if __name__ == "__main__":
     training_args = TrainingArguments(
         output_dir='./results',
         num_train_epochs=3,
-        per_device_train_batch_size=2,
-        per_device_eval_batch_size=2,
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=16,
         eval_strategy="epoch",
         save_strategy="epoch",
         learning_rate=2e-4,
