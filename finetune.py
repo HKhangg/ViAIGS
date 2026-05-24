47da58aa550340aeffc2ab2a6333862b02046dcc
Huy Khang <maihuykhanghaha@gmail.com>
2026-05-24 17:17:03 +0700

    on gradient checkpointing

--- finetune.py
+++ finetune.py
@@ -164,7 +164,8 @@ def run_train(args):
         load_best_model_at_end=True,
         report_to="none",
         remove_unused_columns=False,
-        gradient_checkpointing=False, # gradient_checkpointing_kwargs={"use_reentrant": False}
+        gradient_checkpointing=True, # Bật cái này giảm VRAM nhưng tăng thời gian train
+        gradient_checkpointing_kwargs={"use_reentrant": False},
         warmup_ratio=0.1,
         bf16=True,
         optim="adamw_torch",
