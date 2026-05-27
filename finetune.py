2f33dd71397cd7d67d220ea548efe88028f0b605
Huy Khang <maihuykhanghaha@gmail.com>
2026-05-27 20:05:41 +0700

    change to normal phobert

--- finetune.py
+++ finetune.py
@@ -24,7 +24,7 @@ hf_token = os.getenv("HF_TOKEN", "")
 
 target_map = {
             "microsoft/mdeberta-v3-base": ["query_proj", "key_proj", "value_proj"],
-            "vinai/phobert-base-v2": [
+            "vinai/phobert-base": [
                 "query",
                 "key",
                 "value",
